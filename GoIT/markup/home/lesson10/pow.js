cc5d8dcb01bed4abbb0dac08cdb7f529d207246b
Наталия <kravec_n@ukr.net>
2015-11-30 19:29:44 +0200

    js homework

---
+++ GoIT/markup/home/lesson10/pow.js
@@ -0,0 +1,20 @@
+﻿function pow(x, n) {
+  var result = 1;
+
+  for (var i = 0; i < n; i++){
+  	result *=x;
+  }
+  return result;
+}
+
+var x = prompt('Введите число для возведения в степень:');
+var n = prompt('Введите степень:');
+
+if (n < 0) {
+  n = prompt("Введите положительную степень:");
+  console.log( pow(x, n) );
+} else {
+  //alert('lf');
+   console.log( pow(x, n) );
+  }
+
---
+++ GoIT/markup/home/lesson11/name_array.js
@@ -0,0 +1,26 @@
+﻿var arrNames = new Array;
+for (var i = 0; i < 5; i++) {
+	
+	do {
+		var name = prompt("Введите имя:");
+	}	while ( (!name) || (name==' '));
+
+		arrNames[i] = name;
+}
+
+var newName = prompt("Введите имя для сравнения:");
+var temp;
+
+for (var i = 0; i < arrNames.length; i++) {
+	
+	if ( newName == arrNames[i] ) {
+		alert(newName+', вы успешно вошли.');
+		temp = '1';
+		break;
+	} else {
+		temp = '0';
+	}
+}
+if (temp == '0') {
+	alert('Извините, такой пользователь не зарегистрирован!');
+}
