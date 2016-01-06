583b40903983911edfd0e1db013e321812421023
Наталия <kravec_n@ukr.net>
2016-01-06 17:27:25 +0200

    New version

--- GoIT/javascript/home/lesson-7-8/script.js
+++ GoIT/javascript/home/lesson-7-8/script.js
@@ -1,8 +1,9 @@
 $(function(){
-	var $submenu = $('.submenu');
-	$submenu.hide();
-	$firstSubmenu = $submenu.first();
-	$firstSubmenu.show();
+
+	var $divTabs = $('.wrapper div');
+	$divTabs.hide();
+	$firstTab = $divTabs.first();
+	$firstTab.show();
 	var $linkAll = $('a');
 	$linkAll.addClass('link2');
 	var $linkFirst = $linkAll.first();
@@ -15,41 +16,62 @@ $(function(){
 	$form.append('<div></div>');
 	var $div = $form.children();
 	$div.addClass('inputs'); 
-	$div.append('<div></div>');
-	var $div = $form.children('div');
 	$div
-		.append('<label>Firstname <input></label>')
-		.append('<label>Lastname <input></label>')
-		.append('<label>Address <input></label>');
+		.append('<label>Firstname </label>')
+		.append('<label>Lastname </label>')
+		.append('<label>Address </label>');
 	
-	var $input = $('input');
-	$input.attr('type', 'text');
-	$input.eq(2).css('margin-left', '14px');
 	var $label = $('label');
 	var $firstLabel = $label.eq(0);
-	$firstLabel.append('<span>Please provide your firstname.</span>');
+	//console.log($firstLabel);
+	$firstLabel.attr('for', 'firstname');
 	var $secondLabel = $label.eq(1);
-	$secondLabel.append('<span>Please provide also your lastname.</span>');
+	$secondLabel.attr('for', 'lastname');
 	var $thirdLabel = $label.eq(2);
-	$thirdLabel.append('<span>Your home or work address.</span>');
-	var $tooltip = $('span');
-	$tooltip.hide();
-
+	$thirdLabel.attr('for', 'address');
+	$label.append('<input>');
+	
+	var $input = $('input');
+	var $firstInput =$input.eq(0);
+	var $secondInput =$input.eq(1);
+	var $thirdInput =$input.eq(2);
+	$input.attr('type','text');
+	$firstInput
+			.attr('title', 'Please provide your firstname.')
+			.attr('id', 'firstname');
+	$secondInput
+			.attr('title', 'Please provide also your lastname.')
+			.attr('id', 'lastname');
+	$thirdInput
+			.attr('title', 'Your home or work address.')
+			.attr('id', 'address')
+			.css('margin-left', '14px');
 
 	$form.append('<button>Show help</button>');
 	var $button = $form.children('button');
 	$button.addClass('button');
+	//------------------------------------------------------------------------
+		var $tooltipOne;
+		var $tooltipTwo;
+		var $tooltipThree;
+		
 
 	$linkAll.on('click',function(event){
-		$submenu.hide();
+		$divTabs.hide();
 		$linkFirst.removeClass('link');
 		$linkFirst.addClass('link2');
-		var $activeSubmenu = $(this).siblings('.submenu');
-		$activeSubmenu.toggle();
+		var $value = $(this).attr('id');
+		var $activeDiv = $('div[aria-labelledby='+$value+']'); 
+		$activeDiv.toggle();
 		});
 
 	$input.hover(function(event){
-					var $tooltipActive = $(this).siblings('span');
+				var $span = $('span');
+				$span.remove();
+					var $title = $(this).attr('title');
+					var $activeLabel = $(this).parent();
+					$activeLabel.append('<span><span>');
+					var $tooltipActive = $('span').text($title);
 					$tooltipActive.addClass('tooltipStyle');
 					$tooltipActive.offset(function(i, coord){
 									        var  newCoord = {};	
@@ -61,30 +83,45 @@ $(function(){
 					$tooltipActive.show();
 				}, 
 			function(){
-				var $tooltipActive = $(this).siblings('span');
-				$tooltipActive.hide();
-				$tooltipActive.offset(function(i, coord){
-									        var  newCoord = {};	
-       										newCoord.top = coord.top;	
-        									newCoord.left = coord.left-30;	
-       										 return newCoord;
-   											});
-
+				var $tooltipActive = $('span');
+				$tooltipActive.remove();
 			}
 
 		);
-	$button.one('click', function(event){
-		$tooltip.offset(function(i, coord){
+	$button.on('click', function(event){
+		var $span = $('span');
+		$span.remove();
+		$label.append('<span></span>');
+		var $tooltipSpan = $('span');
+		var $input = $('input');
+		var $tooltipTitleOne = $input.eq(0).attr('title');
+		var $tooltipTitleTwo = $input.eq(1).attr('title');
+		var $tooltipTitleThree = $input.eq(2).attr('title');
+			$tooltipOne = $tooltipSpan.eq(0).text($tooltipTitleOne);
+			$tooltipTwo = $tooltipSpan.eq(1).text($tooltipTitleTwo);
+			$tooltipThree = $tooltipSpan.eq(2).text($tooltipTitleThree);
+		
+		$tooltipOne.offset(function(i, coord){
 									        var  newCoord = {};	
        										newCoord.top = coord.top;	
         									newCoord.left = coord.left + 30;	
        										 return newCoord;
    											});
-		$tooltip.addClass('tooltipStyle');
-
-		$tooltip.show();
-	}
-
-	);
-
+		$tooltipTwo.offset(function(i, coord){
+									        var  newCoord = {};	
+       										newCoord.top = coord.top;	
+        									newCoord.left = coord.left + 30;	
+       										 return newCoord;
+   											});
+		$tooltipThree.offset(function(i, coord){
+									        var  newCoord = {};	
+       										newCoord.top = coord.top;	
+        									newCoord.left = coord.left + 30;	
+       										 return newCoord;
+   											});
+		$tooltipOne.addClass('tooltipStyle');
+		$tooltipTwo.addClass('tooltipStyle');
+		$tooltipThree.addClass('tooltipStyle');
+	});//закончилась button.on() 
+		
 });
