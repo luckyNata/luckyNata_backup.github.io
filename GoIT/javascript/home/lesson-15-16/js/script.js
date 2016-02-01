28e099bd05aa0530399586266b30a53d7d6d39aa
Наталия <kravec_n@ukr.net>
2016-02-01 22:53:16 +0200

    lesson 15-16

---
+++ GoIT/javascript/home/lesson-15-16/js/script.js
@@ -0,0 +1,31 @@
+﻿function GoogleCallback(jqueryObj, data){
+ console.log('data', data);
+ console.log(data.results[1].title);
+}
+$(function(){
+
+  $.ajax({
+   url:'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q='+ 'test' +'&callback=GoogleCallback&context=?',
+   dataType: 'jsonp',
+   method: 'POST',
+   success: function(){
+   	alert('hello');
+   },
+   error: function(){
+   	 	alert('bad request');
+  },
+ });
+  function getData(){
+  	var dataTmpl = $('#tmpl').html();
+	var tmpl = _.template(dataTmpl);
+	//var titleN=data.results[1].title;
+	var info = {
+		title: "Название",
+		description: 'Очень интересная статья',
+		url: 'test.com.ua',
+	}
+
+	var insert = tmpl(info);
+	$('.wrapper').append(insert);
+}
+});
