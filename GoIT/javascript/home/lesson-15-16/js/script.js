5db4c231e3ec8bd610ffbb7ea37c53943e9de1e7
Наталия <kravec_n@ukr.net>
2016-02-02 14:30:28 +0200

    new

--- GoIT/javascript/home/lesson-15-16/js/script.js
+++ GoIT/javascript/home/lesson-15-16/js/script.js
@@ -1,31 +1,35 @@
-﻿function GoogleCallback(jqueryObj, data){
+﻿var dataObject = {};
+function GoogleCallback(jqueryObj, data){
  console.log('data', data);
  console.log(data.results[1].title);
-}
-$(function(){
+ dataObject = data;
+ console.log(dataObject.results.length);
 
-  $.ajax({
-   url:'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q='+ 'test' +'&callback=GoogleCallback&context=?',
-   dataType: 'jsonp',
-   method: 'GET',
-   success: function(){
-   	alert('hello');
-   },
-   error: function(){
-   	 	alert('bad request');
-  },
- });
-
-  	var dataTmpl = $('#tmpl').html();
-	var tmpl = _.template(dataTmpl);
-	//var titleN=data.results[1].title;
-	var info = {
-		title: "Название",
-		description: 'Очень интересная статья',
-		url: 'test.com.ua',
+   		var dataTmpl = $('#tmpl').html();
+		var tmpl = _.template(dataTmpl);
+		var info=[];
+	for (var i=0; i<dataObject.results.length; i++){
+		
+		info[i] = {
+		title:''+dataObject.results[i].title+'',
+		content: ''+dataObject.results[i].content+'',
+		url: ''+dataObject.results[i].url+'',
+		}
+		console.log(info[i]);
 	}
+		var insert = tmpl({dat:info});
+		$('.wrapper').append(insert);
+}
 
-	var insert = tmpl(info);
-	$('.wrapper').append(insert);
+$(function(){
+//	$('.button').on('click', start);
+
+		$.ajax({
+   			url:'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q='+ 'test' +'&callback=GoogleCallback&context=?',
+  			dataType: 'jsonp',
+			method: 'POST',
+			//success: loadData,
+  		});		
 
 });
+
