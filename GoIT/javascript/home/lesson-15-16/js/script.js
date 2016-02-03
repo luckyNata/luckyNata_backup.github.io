5708456562102d832d8273a00e72a945a40ce17a
Наталия <kravec_n@ukr.net>
2016-02-03 02:12:45 +0200

    homework 15-16

--- GoIT/javascript/home/lesson-15-16/js/script.js
+++ GoIT/javascript/home/lesson-15-16/js/script.js
@@ -1,13 +1,11 @@
 ﻿var dataObject = {};
 function GoogleCallback(jqueryObj, data){
- console.log('data', data);
- console.log(data.results[1].title);
  dataObject = data;
- console.log(dataObject.results.length);
 
-   		var dataTmpl = $('#tmpl').html();
-		var tmpl = _.template(dataTmpl);
-		var info=[];
+   	var dataTmpl = $('#tmpl').html();
+	var tmpl = _.template(dataTmpl);
+	var info=[];
+	
 	for (var i=0; i<dataObject.results.length; i++){
 		
 		info[i] = {
@@ -15,21 +13,65 @@ function GoogleCallback(jqueryObj, data){
 		content: ''+dataObject.results[i].content+'',
 		url: ''+dataObject.results[i].url+'',
 		}
-		console.log(info[i]);
+		//console.log(info[i]);
 	}
-		var insert = tmpl({dat:info});
-		$('.wrapper').append(insert);
+	var insert = tmpl({dat:info});
+	$('.wrapper').append(insert);
 }
 
 $(function(){
-//	$('.button').on('click', start);
+	$('#form').on('submit', function(e){
+		var request = $('.input').val();
+		$('.center').removeClass('center').addClass('left');
 
 		$.ajax({
-   			url:'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q='+ 'test' +'&callback=GoogleCallback&context=?',
+   			url:'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=8&q='+ request +'&callback=GoogleCallback&context=?',
   			dataType: 'jsonp',
 			method: 'POST',
-			//success: loadData,
+			//success: ,
   		});		
+  	e.preventDefault();
+});
 
+//-------------Объекты и прототипное наследование ------------------------
+
+ function Human(name, gender){
+ 	this.name = name,
+ 	this.age = 30,
+ 	this.sex = gender,
+ 	this.height = 185,
+ 	this.weight = 80 
+ }
+
+ function Worker(title){
+ 	this.company = title,
+ 	this.salary = 2000;
+ }
+ Worker.prototype = new Human;
+ 
+ Worker.prototype.toWork = function(){
+	alert('Я работаю с 8:00 до 17:00');
+ };
+	
+ function Student(place){
+ 	this.placeOfStuding = place,
+ 	this.scholarship = 100
+ }
+Student.prototype = new Human;
+
+Student.prototype.watchSeries = function(seriesName){
+	alert('Я смотрю мой любимый сериал '+seriesName+'');
+};
+	
+	var Alex = new Worker("Google");
+	var Kate = new Student("KPI");
+
+ // console.log(Kate.placeOfStuding);
+ // console.log(Kate.age);
+ // console.log(Kate.watchSeries("Friends"));
+
+		console.log(Human);
+		console.log(Alex);
+		console.log(Kate);
 });
 
