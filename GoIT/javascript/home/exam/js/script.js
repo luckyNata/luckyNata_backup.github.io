d861305cc386cc0a5844ee7913473f13cfbc5684
Наталия <kravec_n@ukr.net>
2016-03-17 21:07:22 +0200

    new

--- GoIT/javascript/home/exam/js/script.js
+++ GoIT/javascript/home/exam/js/script.js
@@ -20,44 +20,36 @@ var partners = {
         'hobby': 'Based in Chicago. I love playing tennis and loud music.'
         },
 }
-
-   try {
-    alert('1');
+function ajaxRequest(word){
   $.ajax({
     type: 'GET',
-    url: 'http://api.pixplorer.co.uk/image?word=sport&amount=7&size=m',
+    url: 'http://api.pixplorer.co.uk/image?word='+word+'&amount=7&size=m',
     dataType: 'json',
-    success: function (data){
-        for(var i=0; i<data.images.length; i++){
-        $('.grid-item'+i).css({"background-image":"url("+data.images[i].imageurl+")","background-size":"cover"});
-        }
-    },//success
-    error: function (){
-        for(var i=0; i<7; i++){
-        $('.grid-item'+i).css({"background-image":"url(img/sport"+i+".jpg)","background-size":"cover"});
-        }
-    }
-  });//ajax
-
-} catch (err) {
-        alert('2');
-console.log(err);
-  for(var i=0; i<7; i++){
-        $('.grid-item').css({"background-image":"url(img/sport1.jpg)","background-size":"cover"});
-        }
-}
-
-$(function(){
-    var htmlka = $('#partners-list').html();
-    console.log(htmlka);
-    var content = tmpl(htmlka, partners);
-    $('.partners').append(content);   
- 
+    success: function (data){ 
+    var img_tmpl = $('#images-list').html();
+    var img_content = tmpl(img_tmpl, {data: data});
+    $('.grid').remove();
+    $('.images').append(img_content); 
     $('.grid').isotope({//инициализация изотопа
        itemSelector: '.grid-item',
        columnWidth: '.grid-item',
        gutter: 10
     });
-  
+      // $('.grid-item'+i).css({"background-image":"url("+data.images[i].imageurl+")","background-size":"cover"});
+    }//success
+  });//ajax
+  }
+    ajaxRequest("cats");
 
- });//f-n ready
+$(function(){
+  $.support.cors = true;//поддержка ajax для IE
+    var htmlka = $('#partners-list').html();
+    var content = tmpl(htmlka, partners);
+    $('.partners').append(content);   
+
+    $('.form-search').submit( function(event){
+        event.preventDefault();
+        var request = $('.form-interest__input').val();
+        ajaxRequest(request);
+    });   
+ });
