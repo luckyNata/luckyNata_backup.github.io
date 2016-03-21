29ab14611f7bbe3fe5d82b1d92c6fa335f7cd55c
Наталия <kravec_n@ukr.net>
2016-03-21 23:23:46 +0200

    exam

--- GoIT/javascript/home/exam/gulpfile.js
+++ GoIT/javascript/home/exam/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var concatCss = require('gulp-concat-css');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
@@ -7,7 +10,20 @@ var watch = require('gulp-watch');
 gulp.task('scripts', function() {
   return gulp.src('src/js/*.js')
     .pipe(concat('script.main.js'))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('dest/js/'));
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('dest/js/*.js')
+    .pipe(uglify())
+    .pipe(rename("main.min.js"))
+    .pipe(gulp.dest('dest/js/'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('src/css/*.css')
+    .pipe(concatCss("main.css"))
+    .pipe(gulp.dest('dest/css/'));
 });
 
 gulp.task('sass', function () {
