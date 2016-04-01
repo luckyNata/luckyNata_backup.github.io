187231f3feddd253c2e906df538ca03d16a5e70e
Nataliia <kravec_n@ukr.net>
2016-04-01 23:51:10 +0300

    backup

---
+++ GoIT/javascript/home/exam_version/js/data.js
@@ -0,0 +1,10 @@
+var mockData = {
+    "images":[
+    {"imageurl": "img/more/smile1.jpg", "word": "Smile"},
+    {"imageurl": "img/more/smile2.jpg", "word": "Smile"},
+    {"imageurl": "img/more/smile3.jpg", "word": "Smile"},
+    {"imageurl": "img/more/manysmile.png", "word": "Smile"},
+    {"imageurl": "img/more/image1.jpg", "word": "Smile"},
+    {"imageurl": "img/more/image2.jpg", "word": "Smile"},
+    {"imageurl": "img/more/smile3.jpg", "word": "Smile"}
+]};
